a2bbbfd8221f4980119ea2a78f4757db64de5b71
eibadev <github.steadily398@passmail.net>
2025-03-09 12:30:19 +0100

    url

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,17 +1,16 @@
 import BlueskyIcon from '@/components/icons/BlueskyIcon'
 import GithubIcon from '@/components/icons/GithubIcon'
 
-// ADD YOUR SOCIAL NETWORKS HERE
 export const SOCIALNETWORKS = [
 	{
 		name: 'Github',
-		url: 'https://github.com/danielcgilibert/blog-template',
+		url: 'https://github.com/eibadev',
 		icon: GithubIcon
 	},
 
 	{
 		name: 'Bluesky',
-		url: 'https://github.com/danielcgilibert/blog-template',
+		url: 'https://bsky.app/profile/eibadev.bsky.social',
 		icon: BlueskyIcon
 	}
 ] as const
